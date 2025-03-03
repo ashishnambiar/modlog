ee9d701c35bdbc8458a7dedb0fa790a96c2af329
ashishnambiar <ashishleo1998@gmail.com>
2025-03-03 18:52:04 +0530

    HOTFIX: clean up for new flutter version(3.29.0)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.0.5
+- clean up for new flutter version(3.29.0)
+- added `isAvailable` to disable trigger
+
+## 0.0.4
+- BUGFIX: jsonEncoding fixed for `cURL`
+- interceptor recent requests will be on top
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- lib/src/Logger/Presentation/Widgets/floating_logger_button.dart
+++ lib/src/Logger/Presentation/Widgets/floating_logger_button.dart
@@ -31,68 +31,59 @@ class _FloatingLoggerButtonState extends State<FloatingLoggerButton> {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: _animation,
-        child: Center(
-          child: Material(
-            color: Colors.transparent,
-            child: Flow(
-              clipBehavior: Clip.none,
-              delegate: DebugFlowDelegate(_animation),
-              children: [
-                GestureDetector(
-                  onLongPress: () {
-                    widget._animationController.toggle();
+      animation: _animation,
+      child: Center(
+        child: Material(
+          color: Colors.transparent,
+          child: Flow(
+            clipBehavior: Clip.none,
+            delegate: DebugFlowDelegate(_animation),
+            children: [
+              GestureDetector(
+                onLongPress: () {
+                  widget._animationController.toggle();
+                },
+                child: IconButton(
+                  onPressed: () {
+                    widget.loggerController.openLogger(context);
                   },
-                  child: IconButton(
-                    onPressed: () {
-                      widget.loggerController.openLogger(context);
-                    },
-                    icon: const Icon(Icons.bug_report),
-                    style: ButtonStyle(
-                      shadowColor: WidgetStatePropertyAll(
-                        Colors.purple[900],
-                      ),
-                      side: WidgetStatePropertyAll(
-                        BorderSide(
-                          color: Colors.purple[200]!,
-                          width: 2,
-                        ),
-                      ),
-                      backgroundColor:
-                          WidgetStatePropertyAll(Colors.purple[100]),
-                      elevation: const WidgetStatePropertyAll(5),
+                  icon: const Icon(Icons.bug_report),
+                  style: ButtonStyle(
+                    shadowColor: WidgetStatePropertyAll(Colors.purple[900]),
+                    side: WidgetStatePropertyAll(
+                      BorderSide(color: Colors.purple[200]!, width: 2),
                     ),
+                    backgroundColor: WidgetStatePropertyAll(Colors.purple[100]),
+                    elevation: const WidgetStatePropertyAll(5),
                   ),
                 ),
-                ...customActions.map(
-                  (e) => IconButton(
-                    onPressed: () {
-                      e.onTap();
-                    },
-                    icon: Icon(e.icon),
-                    style: ButtonStyle(
-                      shadowColor: WidgetStatePropertyAll(
-                        Colors.purple[900],
-                      ),
-                      backgroundColor: WidgetStatePropertyAll(
-                        Colors.purple[100],
-                      ),
-                      elevation: const WidgetStatePropertyAll(1),
-                    ),
+              ),
+              ...customActions.map(
+                (e) => IconButton(
+                  onPressed: () {
+                    e.onTap();
+                  },
+                  icon: Icon(e.icon),
+                  style: ButtonStyle(
+                    shadowColor: WidgetStatePropertyAll(Colors.purple[900]),
+                    backgroundColor: WidgetStatePropertyAll(Colors.purple[100]),
+                    elevation: const WidgetStatePropertyAll(1),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        builder: (context, child) {
-          return SizedBox(
-            height: (((_animation.value * customActions.length) + 1) * 48) + 10,
-            width: 58,
-            // MediaQuery.of(context).size.width,
-            child: child!,
-          );
-        });
+      ),
+      builder: (context, child) {
+        return SizedBox(
+          height: (((_animation.value * customActions.length) + 1) * 48) + 10,
+          width: 58,
+          // MediaQuery.of(context).size.width,
+          child: child!,
+        );
+      },
+    );
   }
 }
 
@@ -120,9 +111,11 @@ class DebugFlowDelegate extends FlowDelegate {
             0,
           ),
         )
-          ..translate(i == 0 //
-              ? vec.Vector3(0, 0, 0)
-              : vec.Vector3(-(animation.value - 1) * 25, 0, 0))
+          ..translate(
+            i == 0 //
+                ? vec.Vector3(0, 0, 0)
+                : vec.Vector3(-(animation.value - 1) * 25, 0, 0),
+          )
           ..scale(
             i == 0 //
                 ? vec.Vector3(1, 1, 1)
